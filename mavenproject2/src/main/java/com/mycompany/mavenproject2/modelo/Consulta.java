fd03917d6d360224b543d125c5ae2977ae759f62
nmachadoc1-ops <nmachadoc1@unicartagena.edu.co>
2026-09-10 21:29:10 -0500

    composicion medicamentos y consulta

--- mavenproject2/src/main/java/com/mycompany/mavenproject2/modelo/Consulta.java
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/modelo/Consulta.java
@@ -4,10 +4,73 @@
  */
 package com.mycompany.mavenproject2.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Estudiante
  */
 public class Consulta {
+    private String fecha;
+    private String motivo;
+    private Mascota mascota;
+    private Veterinario veterinario;
+    private List<Medicamento> medicamentos;
+
+    public Consulta(String fecha, String motivo, Mascota mascota, Veterinario veterinario) {
+        this.fecha = fecha;
+        this.motivo = motivo;
+        this.mascota = mascota;
+        this.veterinario = veterinario;
+        this.medicamentos = new ArrayList<>();
+        
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getMotivo() {
+        return motivo;
+    }
+
+    public void setMotivo(String motivo) {
+        this.motivo = motivo;
+    }
+
+    public Mascota getMascota() {
+        return mascota;
+    }
+
+    public void setMascota(Mascota mascota) {
+        this.mascota = mascota;
+    }
+
+    public Veterinario getVeterinario() {
+        return veterinario;
+    }
+
+    public void setVeterinario(Veterinario veterinario) {
+        this.veterinario = veterinario;
+    }
+
     
+    
+    public void agregarMedicamento(Medicamento med) {
+        if (med != null) {
+            this.medicamentos.add(med);
+        }
+    }
+
+    public List<Medicamento> getMedicamentos() {
+        return medicamentos;
+    }
 }
+    
+    
+   
--- mavenproject2/src/main/java/com/mycompany/mavenproject2/modelo/Medicamento.java
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/modelo/Medicamento.java
@@ -9,5 +9,31 @@ package com.mycompany.mavenproject2.modelo;
  * @author Estudiante
  */
 public class Medicamento {
+    private String nombre;
+    private String dosis;
+
+    public Medicamento(String nameme, String dosis) {
+        this.nombre = nombre;
+        this.dosis = dosis;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDosis() {
+        return dosis;
+    }
+
+    public void setDosis(String dosis) {
+        this.dosis = dosis;
+    }
+    
+    
+    
     
 }
