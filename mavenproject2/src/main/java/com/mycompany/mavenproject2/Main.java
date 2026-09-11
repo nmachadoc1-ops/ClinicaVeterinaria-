3c2c4883aea6ae7cc535b34f1df5f9b9f1677a03
nmachadoc1-ops <nmachadoc1@unicartagena.edu.co>
2026-09-10 23:59:13 -0500

    ultimo detalle en el main, corrio vio y mostro datos en consola

---
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject2;
+
+import com.mycompany.mavenproject2.controlador.ClinicaController;
+
+public class Main {
+    public static void main(String[] args) {
+        ClinicaController controlador = new ClinicaController();
+        controlador.iniciar();
+    }
+}
