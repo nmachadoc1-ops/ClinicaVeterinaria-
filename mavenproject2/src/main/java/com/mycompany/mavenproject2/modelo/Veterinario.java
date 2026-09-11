3461873e6ce6d1635df6a19e52a413c6e225eb7c
nmachadoc1-ops <nmachadoc1@unicartagena.edu.co>
2026-09-10 21:54:02 -0500

    clase veterinario, terminada

--- mavenproject2/src/main/java/com/mycompany/mavenproject2/modelo/Veterinario.java
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/modelo/Veterinario.java
@@ -9,5 +9,34 @@ package com.mycompany.mavenproject2.modelo;
  * @author Estudiante
  */
 public class Veterinario {
+    private String nombre;
+    private String especialidad;
+
+    public Veterinario(String nombre, String especialidad) {
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
     
+    
+    public void recetar(Consulta consulta, Medicamento medicamento) {
+        if (consulta != null && medicamento != null) {
+            consulta.agregarMedicamento(medicamento);
+        }
 }
+  }  
